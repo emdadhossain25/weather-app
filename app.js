2ca6f77dc2e07bb511fcd9c6c61240585cf49bb0
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-19 15:57:46 +0600

    added foreach for javascript

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const request = require('request');
 
 request({
-    url:'https://reqres.in/api/users?page=2',
+    url: 'https://reqres.in/api/users?page=2',
     json: true
 
 
-}, (error, response, body) =>{
+}, (error, response, body) => {
 
+
+    // iterating foreach function call for all the elements in body type
+    body.data.forEach(function (t) {
+        console.log(`first name : ${t.first_name} last name : ${t.last_name} `);
+    })
     // the data that comes back from http request is called - body
-    console.log(JSON.stringify(response, undefined, 2))
+    // console.log(JSON.stringify(response, undefined, 2))
 
 
     // response sends back the statusCode which related to requesting server
