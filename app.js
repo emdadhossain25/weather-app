726a824c973e98a47e1ef8e14cddb137c9d21b0c
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-19 16:46:40 +0600

    added encodedURIComponent

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const request = require('request');
 
+
+/**
+ * testing the code for yargs
+ */
+
+const yargs = require('yargs');
+const argv = yargs
+    .options({
+        p: {
+            demand: true,
+            alias: 'page',
+            describe: 'page number to fetch',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+
+var encodedAddress = encodeURIComponent(argv.page)
+
+/**
+ * ending of yargs for using commandline alias and passing arguements
+ * example code for running the code: node app.js -p '2'
+ */
+
 request({
-    url: 'https://reqres.in/api/users?page=2',
+    url: `https://reqres.in/api/users?page=${encodedAddress}`,
     json: true
 
 
@@ -10,7 +37,13 @@ request({
 
     // iterating foreach function call for all the elements in body type
     body.data.forEach(function (t) {
+        console.log('-----------------------')
+        console.log('\n')
+        console.log(`id: ${t.id} `);
         console.log(`first name : ${t.first_name} last name : ${t.last_name} `);
+        console.log(`avatar : ${t.avatar}  `);
+        console.log('\n')
+
     })
     // the data that comes back from http request is called - body
     // console.log(JSON.stringify(response, undefined, 2))
