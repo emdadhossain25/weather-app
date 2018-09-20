f562e2220ad1437c6cfc37a38469c3ce5bc0837d
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-20 17:07:05 +0600

    added some code handling request for forecast.io api calling

--- forecaseapi.js
+++ forecaseapi.js
@@ -3,12 +3,15 @@
 const request = require('request');
 request({
     url: 'https://api.darksky.net/forecast/13c8311e65fe7ef6e005924fd641169c/23.777176,90.399452',
-    json : true
-},(error, response, body)=>{
+    json: true
+}, (error, response, body) => {
 
-    if (error){
-
-    }else {
-        console.log(`Current temprature: ${((body.currently.temperature)-32)*.5556} C`)
+    if (error) {
+        console.log('Unable to connect to Forecast.io server.')
+    } else if (response.statusCode === 400) {
+        console.log('Unable to fetch weather')
+    }
+    else if (response.statusCode === 200) {
+        console.log(`Current temprature: ${((body.currently.temperature) - 32) * .5556} C`)
     }
 })
